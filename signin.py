3c0e43cd4a583d79a658f8455f1dff2f24b8207a
ParkanszkiKrisztina <dr.parkanszki.krisztina@gmail.com>
2023-04-09 17:57:01 +0200

    accept_cookies test

---
+++ signin.py
@@ -0,0 +1,3 @@
+from user_data import registered_user
+
+def login(self):
---
+++ test_conduit.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+class TestConduit(object):
+
+    def setup_method(self):
+        s = Service(executable_path=ChromeDriverManager().install())
+        o = Options()
+        o.add_experimental_option("detach", True)
+        self.browser = webdriver.Chrome(service=s, options=o)
+        URL = "http://localhost:1667/#/"
+        self.browser.get(URL)
+
+    def teardown_method(self):
+        self.browser.quit()
+
+    def accept_cookies(self):
+        accept = self.browser.find_element(By.CLASS_NAME, 'cookie__bar__buttons__button--accept')
+        accept.click()
+
+    ## Cookie-k elfogadásának tesztelése
+    def test_accept_cookies(self):
+        self.accept_cookies()
+        time.sleep(1)
+        cookie_container = self.browser.find_elements(By.CSS_SELECTOR, 'footer > .container > #cookie-policy-panel')
+        assert len(cookie_container) == 0
+
+    def test_register(self):
+
